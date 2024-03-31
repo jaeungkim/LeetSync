6f05128d22cd59220f3070ed2266c5b6a5ef7517
Jaeung Kim <43080200+jaeungkim@users.noreply.github.com>
2024-03-31 12:36:26 +0900

    Time: 53 ms (70.25%) | Memory: 50.2 MB (30.54%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.ts
+++ 70-climbing-stairs/climbing-stairs.ts
@@ -1,15 +1,16 @@
 function climbStairs(n: number): number {
-    if (n === 1) {
+    if (n === 1 || n === 0) {
         return 1;
     }
-    
-    let dp: number[] = new Array(n + 1);
+
+    let dp = new Array(n+1);
+
+    dp[0] = 1;
     dp[1] = 1;
-    dp[2] = 2;
-    
-    for (let i = 3; i <= n; i++) {
-        dp[i] = dp[i - 1] + dp[i - 2];
+
+    for(let i = 2; i <= n; i++){
+        dp[i] = dp[i-1] + dp[i-2];
     }
-    
+
     return dp[n];
 }
