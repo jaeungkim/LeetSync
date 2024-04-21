cd1b0292f62efab1f21a6f8aeb6189c95e8a797c
Jaeung Kim <43080200+jaeungkim@users.noreply.github.com>
2024-04-21 21:31:34 +0900

    Time: 100 ms (52.44%) | Memory: 54.2 MB (68.09%) - LeetSync

--- 561-array-partition/array-partition.js
+++ 561-array-partition/array-partition.js
@@ -7,7 +7,7 @@ var arrayPairSum = function (nums) {
     nums.sort((a, b) => a - b);
     
     let sum = 0;
-    [1,2,3,4]
+
     // Since the array is sorted, we can take every other number starting from index 0
     for (let i = 0; i < nums.length; i += 2) {
         sum += nums[i];
