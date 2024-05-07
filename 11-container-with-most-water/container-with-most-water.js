849544e144304f74d7e2669913f4662e6bdc1ff7
Jaeung Kim <43080200+jaeungkim@users.noreply.github.com>
2024-05-07 20:47:39 +0900

    Time: 73 ms (29.27%) | Memory: 57.6 MB (16.91%) - LeetSync

---
+++ 11-container-with-most-water/container-with-most-water.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    let max = 0;
+    let left = 0;
+    let right = height.length - 1;
+
+    while (left < right) {
+        const h = Math.min(height[left], height[right]);
+        const w = right - left;
+        const area = h * w;
+        max = Math.max(max, area);
+        
+        // Move the pointer pointing to the smaller height inward
+        if (height[left] < height[right]) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+
+    return max;
+};
