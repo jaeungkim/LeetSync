a391b38476a8beefe2b62023e0b3d8ec42816e45
Jaeung Kim <43080200+jaeungkim@users.noreply.github.com>
2024-09-01 21:36:34 +0900

    Time: 64 ms (31.23%) | Memory: 49.5 MB (84.37%) - LeetSync

---
+++ 3476-find-minimum-operations-to-make-all-elements-divisible-by-three/find-minimum-operations-to-make-all-elements-divisible-by-three.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minimumOperations = function(nums) {
+    let count = 0;
+
+    for(let i = 0; i < nums.length; i++) {
+        if(nums[i] % 3 === 0){
+            continue;
+        }
+        count++;
+    }
+
+    return count;
+};
